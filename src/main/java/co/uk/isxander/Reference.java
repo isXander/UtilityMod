1f6ebdb8379f628e0279f0afb7718b6d58b85154
isXander <43245524+isXander@users.noreply.github.com>
2020-06-30 12:08:49 +0100

    Updated version number (New AntiSnipe features)

--- src/main/java/co/uk/isxander/Reference.java
+++ src/main/java/co/uk/isxander/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
 
     public static final String MOD_ID = "isxanderutils";
     public static final String MOD_NAME = "isXander's Utility Mod";
-    public static final String VERSION = "0.5.6";
+    public static final String VERSION = "0.5.7";
     public static final String AUTHOR = "isXander";
 
     public static String getGradleVersion() {
