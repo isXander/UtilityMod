9b5771f3091b6bd887432fe65268d895ba6afa2b
Saalty <43245524+Saalty@users.noreply.github.com>
2020-07-12 11:50:38 +0100

    Added particlemod & renamed packages, classes etc

--- src/main/java/co/uk/isxander/isxanderutils/utilities/autowarp/AutoWarpListener.java
+++
@@ -1,8 +0,0 @@
-package co.uk.isxander.isxanderutils.utilities.autowarp;
-
-public class AutoWarpListener {
-//    @SubscribeEvent
-//    public void worldLoad(WorldEvent.Load event) {
-//        MinecraftForge.EVENT_BUS.register(new AutoWarpTickHandler());
-//    }
-}
